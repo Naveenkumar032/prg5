ef101ca1de9d2fe788832921c986a7bcad1c6f3a
NaveenKumar <naveen196002@gmail.com>
2026-09-18 13:16:46 +0530

    Update student_solution.sh

--- student_solution.sh
+++ student_solution.sh
@@ -5,29 +5,32 @@
 # Register Number:
 
 echo "===== SELinux Status ====="
-
+sestatus
 
 echo "===== Creating Web Directory ====="
-
+mkdir -p /var/www/html/testdir
 echo "===== Creating HTML File ====="
-
+echo "<h1>SELinux Test Page</h1>" > /var/www/html/testdir/index.html
 
 echo "===== Setting Linux Permissions ====="
-
+chmod 755 /var/www/html/testdir
+chmod 644 /var/www/html/testdir/index.html
 
 echo "===== Checking Initial Context ====="
-
+ls -Z /var/www/html/testdir/index.html
 
 echo "===== Assigning Wrong SELinux Context ====="
-
+chcon -t user_home_t /var/www/html/testdir/index.html
 echo "===== Checking Wrong Context ====="
-
+ls -Z /var/www/html/testdir/index.html
 
 echo "===== Checking AVC Denials ====="
+ausearch -m avc -ts recent || grep "denied" /var/log/audit/audit.log | tail -n 10
 echo "===== Correcting SELinux Context ====="
-
+restorecon -v /var/www/html/testdir/index.html
 
 echo "===== Checking Correct Context ====="
-
+ls -Z /var/www/html/testdir/index.html
 
 echo "===== Practical Completed ====="
+
